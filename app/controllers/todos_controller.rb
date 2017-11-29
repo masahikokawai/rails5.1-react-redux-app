0f35d30876a8a751ae2da54bfe7146935d8a0fb7
masahikokawai <masahikokawai81@gmail.com>
2017-11-29 15:51:30 +0900

    Add dummy Todos and fix ".babelrc" file

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,11 @@
 class TodosController < ActionController::Base
+  layout "application"
 
   def index
+    @todos = [
+      { id: 1, text: 'aaa' },
+      { id: 2, text: 'bbb' },
+      { id: 3, text: 'ccc' }
+    ]
   end
 end
