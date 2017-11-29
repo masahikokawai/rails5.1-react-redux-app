4fa6810f7537698ca6de1e3613a87d55e666187c
masahikokawai <masahikokawai81@gmail.com>
2017-11-29 16:43:03 +0900

    Add select from db

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,10 +2,6 @@ class TodosController < ActionController::Base
   layout "application"
 
   def index
-    @todos = [
-      { id: 1, text: 'aaa' },
-      { id: 2, text: 'bbb' },
-      { id: 3, text: 'ccc' }
-    ]
+    @todos = Todo.all
   end
 end
