165194ef2de0427ee17742dc2136bc9ae374bd7e
masahikokawai <masahikokawai81@gmail.com>
2017-11-29 16:54:48 +0900

    Add todos#create and mirror-creator
    bin/yarn add mirror-creator

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -4,4 +4,22 @@ class TodosController < ActionController::Base
   def index
     @todos = Todo.all
   end
+
+  def create
+    @todo = Todo.create!(todo_params)
+    respond_to do |format|
+      format.html {
+        redirect_to root_path
+      }
+      format.json {
+        head 204
+      }
+    end
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:text)
+  end
 end
